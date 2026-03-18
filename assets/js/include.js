6cc1fc87c453e4cb5a966e415fb9663cf8459d01
Durshan Perera <durshan20030102@gmail.com>
2026-03-18 17:45:16 +0530

    Initial commit - MarryMate project

---
+++ assets/js/include.js
@@ -0,0 +1,8 @@
+function loadComponent(id, file){
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch(error => console.error("Error loading component:", error));
+}
